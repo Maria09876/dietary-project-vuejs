c5b597f5ce5d11f7e2dc042f51809781873c3cb3
Maria Efthymiou <102525142+Maria09876@users.noreply.github.com>
2023-08-29 16:54:20 +0200

    chore:remove carousel to slideshow

--- src/pages/HomeCarousel.vue
+++ src/pages/HomeCarousel.vue
@@ -1,30 +1,73 @@
 <template>
-  <div id="carousel" style="margin-top:10%">
-    <div
-      :class="getItemClass(index)"
-      @click="moveToSelected(index)"
-      v-for="(item, index) in items"
-      :key="index"
-      class="customCards"
-    >
-      <img :src="item.src" class="card-img-top customCardsImg" :alt="item.title" />
-      <div class="card-body">
-        <h5 class="card-title">{{ item.title }}</h5>
-        <p class="card-text">
-          {{ item.description }}
-        </p>
-        <a href="#" class="btn btn-primary">Go somewhere</a>
+  <div class="carousel slide" data-ride="carousel" id="cardCarousel">
+    <div class="carousel-inner">
+      <div class="cardWrapper">
+        <div class="card-group container">
+          <div
+            class="card"
+            v-for="(item, index) in filteredItems"
+            :key="index"
+            :class="{ 'carousel-item': true, active: index === 0 }"
+          >
+            <div class="content">
+              <div class="imgBox">
+                <img
+                  :src="item.src"
+                  class="rounded card-img"
+                  :alt="item.title"
+                />
+              </div>
+              <div class="card-body contentBox">
+                <h5 class="card-title">{{ item.title }}</h5>
+                <p class="card-text">
+                  {{ item.description }}
+                </p>
+              </div>
+            </div>
+            <ul class="sci">
+              <li>
+                {{ item.duration }}
+              </li>
+              <li>
+                {{ item.difficulty }}
+              </li>
+              <li>
+                {{ item.quantity }}
+              </li>
+            </ul>
+          </div>
+        </div>
       </div>
     </div>
-    <div class="buttons">
-    <button id="prev" @click="moveToSelected('prev')">Prev</button>
-    <button id="next" @click="moveToSelected('next')">Next</button>
+  </div>
+  <div style="">
+    <div class="progress mt-3" style="margin-left: 8%; margin-right: 16%; height: 4px; ">
+    <div
+      class="progress-bar"
+      role="progressbar"
+      :style="{ width: progress + '%' }"
+      :aria-valuenow="progress"
+      aria-valuemin="0"
+      aria-valuemax="100"
+      style="background-color: rgb(182, 164, 240);"
+    ></div>
+  </div>
+  <div class="buttons">
+    <button id="prev" @click="moveToSelected('prev')">
+      <i class="bi bi-arrow-left"></i>
+    </button>
+    <button id="next" @click="moveToSelected('next')">
+      <i class="bi bi-arrow-right"></i>
+    </button>
   </div>
   </div>
   
 </template>
 
 <script>
+import $ from "jquery";
+const itemsToShow = 4;
+
 export default {
   name: "HomeCarousel",
   data() {
@@ -34,192 +77,303 @@ export default {
         {
           src: require("../assets/breakfast.jpg"),
           title: "Breakfast",
-          description: "for a great start of your day!",
+          description: "yummi!",
+          duration: "20'",
+          difficulty: "Easy",
+          quantity: "6-8",
         },
         {
           src: require("../assets/fish.jpg"),
           title: "fish",
           description: "for a great start of your day!",
+          duration: "20'",
+          difficulty: "Easy",
+          quantity: "6-8",
         },
         {
           src: require("../assets/meat.jpg"),
           title: "meat",
           description: "for a great start of your day!",
+          duration: "20'",
+          difficulty: "Easy",
+          quantity: "6-8",
         },
         {
           src: require("../assets/pasta.jpg"),
           title: "pasta",
           description: "for a great start of your day!",
+          duration: "20'",
+          difficulty: "Easy",
+          quantity: "6-8",
         },
         {
           src: require("../assets/salads.jpg"),
           title: "salads",
           description: "for a great start of your day!",
+          duration: "20'",
+          difficulty: "Easy",
+          quantity: "6-8",
         },
         {
           src: require("../assets/smoothie.jpg"),
           title: "smoothie",
           description: "for a great start of your day!",
+          duration: "20'",
+          difficulty: "Easy",
+          quantity: "6-8",
         },
         {
           src: require("../assets/breakfast.jpg"),
           title: "Breakfast",
           description: "for a great start of your day!",
+          duration: "20'",
+          difficulty: "Easy",
+          quantity: "6-8",
         },
         {
           src: require("../assets/fish.jpg"),
           title: "fish",
           description: "for a great start of your day!",
+          duration: "20'",
+          difficulty: "Easy",
+          quantity: "6-8",
         },
         {
           src: require("../assets/meat.jpg"),
           title: "meat",
           description: "for a great start of your day!",
+          duration: "20'",
+          difficulty: "Easy",
+          quantity: "6-8",
         },
         {
           src: require("../assets/pasta.jpg"),
           title: "pasta",
           description: "for a great start of your day!",
+          duration: "20'",
+          difficulty: "Easy",
+          quantity: "6-8",
         },
         {
           src: require("../assets/salads.jpg"),
           title: "salads",
           description: "for a great start of your day!",
+          duration: "20'",
+          difficulty: "Easy",
+          quantity: "6-8",
         },
         {
           src: require("../assets/smoothie.jpg"),
           title: "smoothie",
           description: "for a great start of your day!",
+          duration: "20'",
+          difficulty: "Easy",
+          quantity: "6-8",
         },
+        {
+          src: require("../assets/breakfast.jpg"),
+          title: "Breakfast",
+          description: "yummi!",
+          duration: "20'",
+          difficulty: "Easy",
+          quantity: "6-8",
+        },
+        {
+          src: require("../assets/fish.jpg"),
+          title: "fish",
+          description: "for a great start of your day!",
+          duration: "20'",
+          difficulty: "Easy",
+          quantity: "6-8",
+        },
+        {
+          src: require("../assets/meat.jpg"),
+          title: "meat",
+          description: "for a great start of your day!",
+          duration: "20'",
+          difficulty: "Easy",
+          quantity: "6-8",
+        },
+        {
+          src: require("../assets/pasta.jpg"),
+          title: "pasta",
+          description: "for a great start of your day!",
+          duration: "20'",
+          difficulty: "Easy",
+          quantity: "6-8",
+        },
+        {
+          src: require("../assets/salads.jpg"),
+          title: "salads",
+          description: "for a great start of your day!",
+          duration: "20'",
+          difficulty: "Easy",
+          quantity: "6-8",
+        },
+        {
+          src: require("../assets/smoothie.jpg"),
+          title: "smoothie",
+          description: "for a great start of your day!",
+          duration: "20'",
+          difficulty: "Easy",
+          quantity: "6-8",
+        }
       ],
+      progress: 0,
     };
   },
   methods: {
-    getItemClass(index) {
-      if (index === this.selectedIndex) return 'selected';
-      if (index === this.selectedIndex - 1) return 'prev';
-      if (index === this.selectedIndex + 1) return 'next';
-      if (index === this.selectedIndex - 2) return 'prevLeftSecond';
-      if (index === this.selectedIndex + 2) return 'nextRightSecond';
-      if (index < this.selectedIndex - 2) return 'hideLeft';
-      if (index > this.selectedIndex + 2) return 'hideRight';
-      return '';
-    },
     moveToSelected(element) {
-      if (element === "next" && this.selectedIndex < this.items.length - 1) {
-        this.selectedIndex++;
-      } else if (element === "prev" && this.selectedIndex > 0) {
-        this.selectedIndex--;
-      } else if (typeof element === 'number') {
-        this.selectedIndex = element;
-      }
-    },
-    mounted() {
-    document.addEventListener("keydown", this.handleKeydown);
-  },
-  beforeUnmount() {
-    document.removeEventListener("keydown", this.handleKeydown);
-  },
- 
-    handleKeydown(e) {
-      if (e.keyCode === 37) {
-        this.moveToSelected('prev');
-      } else if (e.keyCode === 39) {
-        this.moveToSelected('next');
-      }
-    },
-  },
-  
-};
+      const step = 100 / ( ( this.items.length - itemsToShow ) / itemsToShow ); // Calculate progress step based on items per view (4 items per view)
 
+      if (element === "next" && this.selectedIndex < this.items.length - itemsToShow) {
+    this.selectedIndex = this.selectedIndex + itemsToShow
+    this.progress += step;
+  } else if (element === "prev" && this.selectedIndex > 0) {
+    this.selectedIndex =this.selectedIndex - itemsToShow;
+    this.progress -= step;
+  } 
+    },
+  },
+  mounted() {
+    $("#cardCarousel").on("slid.bs.carousel", (e) => {
+      this.progress = ((e.to + 1) / this.items.length) * 100;
+    });
+  },
+  computed: {
+    filteredItems() {
+      return this.items.slice(
+        this.selectedIndex,
+        this.selectedIndex + itemsToShow
+      );
+    },
+  },
+};
 </script>
 
 <style scoped>
-/* cover-flow like effect, where each card appears to be on top of the next one, and the center card is the most prominent */
-#carousel {
+.cardWrapper {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-wrap: wrap;
+}
+.container {
   position: relative;
-  width:80%;
+  z-index: 1;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-wrap: wrap;
+  margin: 40px 0;
+}
+.container .card {
+  position: relative;
+  width: 300px;
   height: 500px;
-  left:10%;
-  transform: translateY(-50%);
-  background-color: bisque;
+  margin: 20px;
+  box-shadow: 0 15px 35px rgba(0, 0, 0, 0.5);
+  border-radius: 15px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  overflow: hidden; /* Add this to prevent inner content from spilling out */
 }
-#carousel div {
+.container .card .content {
+  /*position: relative;
+   display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column; */
+  transition: transform 0.7s ease-in-out;
+  width: 100%;
+  height: 100%;
+}
+
+.container .card:hover .content {
+  transform: translateY(-20px);
+}
+
+.container .card .content .imgBox {
+  /* position: relative;
+  width: 150px;
+  height: 150px;
+  overflow: hidden; */
   position: absolute;
-  transition: transform 1s, left 1s, opacity 2s, z-index 0s;
-  opacity: 2;
-}
-#carousel div img {
-  width: 400px;
-  transition: width 1s;
-}
-#carousel div.hideLeft {
-  left: 0%;
-  opacity: 0;
-  transform: translateY(50%) translateX(-50%);
-}
-#carousel div.hideLeft img {
-  width: 200px;
-}
-#carousel div.hideRight {
-  left: 100%;
-  opacity: 0;
-  transform: translateY(50%) translateX(-50%);
-}
-#carousel div.hideRight img {
-  width: 200px;
-}
-#carousel div.prev {
-  z-index: 5;
-  left: 30%;
-  transform: translateY(50px) translateX(-50%);
-}
-#carousel div.prev img {
-  width: 300px;
-}
-#carousel div.prevLeftSecond {
-  z-index: 4;
-  left: 15%;
-  transform: translateY(50%) translateX(-50%);
-  opacity: 0.7;
-}
-#carousel div.prevLeftSecond img {
-  width: 200px;
-}
-#carousel div.selected {
-  z-index: 10;
-  left: 50%;
-  transform: translateY(0px) translateX(-50%);
-}
-#carousel div.next {
-  z-index: 5;
-  left: 70%;
-  transform: translateY(50px) translateX(-50%);
-}
-#carousel div.next img {
-  width: 300px;
-}
-#carousel div.nextRightSecond {
-  z-index: 4;
-  left: 85%;
-  transform: translateY(50%) translateX(-50%);
-  opacity: 0.7;
-}
-#carousel div.nextRightSecond img {
-  width: 200px;
-}
-.customCards{
-    border: 2px solid aqua;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  overflow: hidden;
+  z-index: 1; /* place the image below the text content */
 }
 
-.card-body{
-    text-align: center;
+.container .card .content .imgBox img {
+  /* position: absolute;
+  top: 0;
+  left: 0; */
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+  transition: opacity 0.7s ease-in-out;
 }
 
-.buttons {
-  position: fixed;
-  left: 50%;
-  transform: translateX(-50%);
-  bottom:0;
+.container .card .content .contentBox {
+  /* color: black;
+  text-transform: uppercase;
+  letter-spacing: 2px;
+  font-weight: 500;
+  font-size: 18px;
+  text-align: center;
+  margin: 20px 0 10px;
+  line-height: 1.1em; */
+  color: white;
+  text-shadow: 1.5px 0px black;
+  font-weight: 500;
+  position: relative;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-end;
+  align-items: center;
+  z-index: 2; /* Place the content above the image */
+  transition: opacity 0.3s ease; /* Smooth transition */
+}
+
+.container .card .content .contentBox h5 p {
+  font-size: 12px;
+  font-weight: 300;
+  text-transform: initial;
+}
+
+.container .card .sci {
+  position: relative;
+  bottom: 5px;
+  display: flex;
+  padding: 0;
+  margin: 0;
+}
+
+.container .card .sci li {
+  list-style: none;
+  margin: 0 10px;
+  transition: 0.5s;
+  opacity: 0;
+}
+.container .card:hover .content .contentBox {
+  opacity: 1; /* Display the content on hover */
+  transition: opacity 0.7s ease-out;
+}
+.container .card:hover .sci li {
+  transform: translateY(0px);
+  opacity: 1;
+}
+
+.container .card .sci li {
+  color: black;
+  font-size: 20px;
 }
 </style>
